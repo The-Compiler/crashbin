017c7b57c96e739b63462bce18bcf84e09eb4b6f
Florian Bruhin <me@the-compiler.org>
2019-04-08 14:53:43 +0200

    Add basic models

--- crashbin_app/admin.py
+++ crashbin_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
+
+admin.site.register(models.Label)
+admin.site.register(models.Bin)
+admin.site.register(models.Report)
 
 # Register your models here.
--- crashbin_app/models.py
+++ crashbin_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
-# Create your models here.
+
+class Label(models.Model):
+
+    name = models.CharField(max_length=255)
+    # FIXME color-field using django-color{ful,field}?
+    color = models.CharField(max_length=7)
+    description = models.TextField()
+
+
+class Bin(models.Model):
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    subscribers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                         related_name='subscribed_bins')
+    maintainers = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                         related_name='maintained_bins')
+    labels = models.ManyToManyField(Label)
+    related_bins = models.ManyToManyField('self')
+
+
+class Report(models.Model):
+
+    email = models.EmailField()
+    created_date = models.DateTimeField(default=timezone.now)
+    bin = models.ForeignKey(Bin, on_delete=models.CASCADE)
+    labels = models.ManyToManyField(Label)
