0b1eda3305dcfbc29575f112652f23d774721c0e
Florian Bruhin <me@the-compiler.org>
2019-04-09 09:43:45 +0200

    Improve current models

--- crashbin_app/models.py
+++ crashbin_app/models.py
@@ -8,24 +8,38 @@ class Label(models.Model):
     name = models.CharField(max_length=255)
     # FIXME color-field using django-color{ful,field}?
     color = models.CharField(max_length=7)
-    description = models.TextField()
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 
 class Bin(models.Model):
 
     name = models.CharField(max_length=255)
-    description = models.TextField()
+    description = models.TextField(blank=True)
     subscribers = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                         related_name='subscribed_bins')
+                                         related_name='subscribed_bins',
+                                         blank=True)
     maintainers = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                         related_name='maintained_bins')
-    labels = models.ManyToManyField(Label)
-    related_bins = models.ManyToManyField('self')
+                                         related_name='maintained_bins',
+                                         blank=True)
+    labels = models.ManyToManyField(Label, blank=True)
+    related_bins = models.ManyToManyField('self', blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
 
 
 class Report(models.Model):
 
-    email = models.EmailField()
-    created_date = models.DateTimeField(default=timezone.now)
+    email = models.EmailField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
     bin = models.ForeignKey(Bin, on_delete=models.CASCADE)
-    labels = models.ManyToManyField(Label)
+    log = models.TextField(blank=True)
+    labels = models.ManyToManyField(Label, blank=True)
+
+    def __str__(self):
+        return self.created_at.ctime()
