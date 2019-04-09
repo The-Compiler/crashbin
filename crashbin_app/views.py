6f5948d6e71ac629ae9da6a4476f89761598d542
Florian Bruhin <me@the-compiler.org>
2019-04-09 11:15:01 +0200

    Show reports in template

--- crashbin_app/views.py
+++ crashbin_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from .models import Report
+
 
 def home(request):
-    return render(request, 'crashbin_app/home.html', {})
+    reports = Report.objects.order_by('created_at')
+    return render(request, 'crashbin_app/home.html', {'reports': reports})
