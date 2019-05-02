c12a5d819c730bd1a2053e4433ba3c00945cfad1
Florian Bruhin <me@the-compiler.org>
2019-05-02 14:35:49 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='crashbin',
+    version='0.0.0',
+    url='https://github.com/The-Compiler/crashbin/',
+    description='Crash collecting and reporting',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Bug Tracking',
+    ],
+    install_requires=[
+        'django',
+        'django-crispy-forms',
+    ]
+)
