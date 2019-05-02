de011c9605f2f1bd956f5526856e6bff57bf8824
Florian Bruhin <me@the-compiler.org>
2019-05-02 15:40:08 +0200

    Add mypy

--- crashbin_app/views.py
+++ crashbin_app/views.py
@@ -1,43 +1,44 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpRequest, HttpResponse
 
 from .models import Report, Bin
 from .forms import BinForm
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
     bins = Bin.objects.order_by('created_at')
     return render(request,
                   'crashbin_app/home.html',
                   {'bins': bins})
 
 
-def report_list(request):
+def report_list(request: HttpRequest) -> HttpResponse:
     reports = Report.objects.order_by('created_at')
     return render(request,
                   'crashbin_app/reports.html',
                   {'reports': reports})
 
 
-def report_detail(request, pk):
+def report_detail(request: HttpRequest, pk: int) -> HttpResponse:
     report = get_object_or_404(Report, pk=pk)
     return render(request, 'crashbin_app/report_detail.html',
                   {'report': report})
 
 
-def bin_list(request):
+def bin_list(request: HttpRequest) -> HttpResponse:
     bins = Bin.objects.order_by('created_at')
     return render(request,
                   'crashbin_app/bins.html',
                   {'bins': bins})
 
 
-def bin_detail(request, pk):
+def bin_detail(request: HttpRequest, pk: int) -> HttpResponse:
     bin_obj = get_object_or_404(Bin, pk=pk)
     return render(request, 'crashbin_app/bin_detail.html',
                   {'bin': bin_obj})
 
 
-def bin_new(request):
+def bin_new(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = BinForm(request.POST)
         if form.is_valid():
