88b0ebf66c8590b4546f1257597a90af3f9af746
Florian Bruhin <me@the-compiler.org>
2019-05-06 17:39:08 +0200

    Add report list component

--- crashbin_app/templatetags/components.py
+++ crashbin_app/templatetags/components.py
@@ -13,6 +13,16 @@ def bin_list(bins, prefix=''):
     return {'bins': bins, 'prefix': prefix}
 
 
+@register.inclusion_tag('crashbin_app/report_list_component.html')
+def report_list(reports, prefix=''):
+    """Generate a list showing the given report and its contents.
+
+    If 'prefix' is given, it's prepended to HTML ids so that the component can
+    be used multiple times on the same page without conflicting IDs.
+    """
+    return {'reports': reports, 'prefix': prefix}
+
+
 @register.inclusion_tag('crashbin_app/search_component.html')
 def search():
     """Generate a bin/report search box."""
