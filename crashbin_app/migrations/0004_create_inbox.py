ce0a443f434359d1027d556480405d36d6c226cc
Florian Bruhin <me@the-compiler.org>
2019-05-06 15:30:19 +0200

    Use inbox bin when none is given

---
+++ crashbin_app/migrations/0004_create_inbox.py
@@ -0,0 +1,18 @@
+from django.db import migrations
+
+
+def create_inbox(apps, schema_editor):
+    Bin = apps.get_model('crashbin_app', 'Bin')
+    Bin.objects.create(name='Inbox',
+                       description='Default inbox bin for new reports.')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('crashbin_app', '0003_related_name'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_inbox)
+    ]
