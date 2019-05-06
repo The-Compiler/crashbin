dcbab144e5b3295a5e1879d8978e5628c2ebf78c
Florian Bruhin <me@the-compiler.org>
2019-05-06 18:06:57 +0200

    Use real data for home view

--- crashbin_app/views.py
+++ crashbin_app/views.py
@@ -6,10 +6,13 @@ from .forms import BinForm
 
 
 def home(request: HttpRequest) -> HttpResponse:
-    bins = Bin.objects.order_by('created_at')
-    return render(request,
-                  'crashbin_app/home.html',
-                  {'bins': bins})
+    data = {
+        'bins': Bin.objects.order_by('created_at'),
+        'reports': Report.objects,
+        'maintained_bins': Bin.objects.filter(maintainers=request.user),
+        'subscribed_bins': Bin.objects.filter(subscribers=request.user),
+    }
+    return render(request, 'crashbin_app/home.html', data)
 
 
 def report_list(request: HttpRequest) -> HttpResponse:
