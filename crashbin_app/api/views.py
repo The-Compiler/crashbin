c842f9fbe4813e21c5351781769a0a644ba2d1e6
Florian Bruhin <me@the-compiler.org>
2019-05-06 16:21:20 +0200

    Remove comments

--- crashbin_app/api/views.py
+++ crashbin_app/api/views.py
@@ -1,5 +1,4 @@
 from crashbin_app.api.serializers import ReportSerializer
-# from crashbin_app.models import Report
 
 from rest_framework import mixins, permissions, generics
 from django.views.decorators.csrf import csrf_exempt
@@ -8,8 +7,6 @@ from django.views.decorators.csrf import csrf_exempt
 class ReportNew(mixins.CreateModelMixin, generics.GenericAPIView):
 
     permission_classes = (permissions.AllowAny,)
-
-    # queryset = Report.objects.all()
     serializer_class = ReportSerializer
 
     @csrf_exempt
