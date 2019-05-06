a7b4cf98d92b40da71bad7eb553b96c66df97c51
Florian Bruhin <me@the-compiler.org>
2019-05-06 16:21:49 +0200

    Remove unneeded csrf_exempt

--- crashbin_app/api/views.py
+++ crashbin_app/api/views.py
@@ -1,7 +1,6 @@
 from crashbin_app.api.serializers import ReportSerializer
 
 from rest_framework import mixins, permissions, generics
-from django.views.decorators.csrf import csrf_exempt
 
 
 class ReportNew(mixins.CreateModelMixin, generics.GenericAPIView):
@@ -9,6 +8,5 @@ class ReportNew(mixins.CreateModelMixin, generics.GenericAPIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = ReportSerializer
 
-    @csrf_exempt
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
