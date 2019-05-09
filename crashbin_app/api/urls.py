f6a0d8b745e36f20f90d84722c79db4ad4035bf9
Luca Tavernini <tavernoshca@gmail.com>
2019-05-09 18:41:29 +0200

    Add bin and report api

--- crashbin_app/api/urls.py
+++ crashbin_app/api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from crashbin_app.api import views
 
 
+router = routers.DefaultRouter()
+router.register(r'reports', views.ReportViewSet)
+router.register(r'bins', views.BinViewSet)
+
 urlpatterns = [
-    path('report/new/', views.ReportNew.as_view()),
+    path('', include(router.urls)),
+    path('report/new/', views.ReportNew.as_view())
 ]
