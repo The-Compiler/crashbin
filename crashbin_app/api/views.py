cee860296403682a0a93ebecce7ba1e0d9ee7479
Luca Tavernini <tavernoshca@gmail.com>
2019-05-14 12:22:40 +0200

    Implement subscribe function

--- crashbin_app/api/views.py
+++ crashbin_app/api/views.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets, mixins, permissions, generics
+from django.http import HttpRequest, HttpResponse
+from rest_framework import viewsets, mixins, permissions, generics, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from crashbin_app.api.serializers import ReportSerializer, ReportNewSerializer, BinSerializer
 from crashbin_app.models import Report, Bin
 
@@ -19,3 +23,15 @@ class ReportNew(mixins.CreateModelMixin, generics.GenericAPIView):
 class BinViewSet(viewsets.ModelViewSet):
     queryset = Bin.objects.all()
     serializer_class = BinSerializer
+
+@api_view(['POST'])
+def bin_subscribe(request: HttpRequest, pk: int) -> HttpResponse:
+    if request.method == 'POST':
+        user = request.user  # type: ignore
+        bin: Bin = Bin.objects.get(id=pk)
+        if user in bin.subscribers.all():
+            bin.subscribers.remove(user)
+        else:
+            bin.subscribers.add(user)
+        return Response(status=status.HTTP_202_ACCEPTED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
