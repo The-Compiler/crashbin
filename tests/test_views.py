384d8725769f3373e6bb4ee81092651864379f06
Florian Bruhin <me@the-compiler.org>
2019-05-14 17:46:34 +0200

    Add some tests for searches

--- tests/test_views.py
+++ tests/test_views.py
@@ -13,11 +13,22 @@ def test_home(admin_client):
     assert b'Bins you maintain' in response.content
 
 
-def test_report_list(admin_client, report_obj):
-    response = admin_client.get(urls.reverse('report_list'))
+@pytest.mark.parametrize('query, matches', [
+    (None, True),
+    ('test', True),
+    ('Test', True),
+    ('', True),
+    ('blah', False),
+])
+def test_report_list(admin_client, report_obj, query, matches):
+    url = urls.reverse('report_list')
+    if query is not None:
+        url += '?q={}'.format(query)
+
+    response = admin_client.get(url)
     assert response.status_code == 200
-    assert b'>testreport<' in response.content
-    assert b'href="/report/1/"' in response.content
+    assert (b'>testreport<' in response.content) == matches
+    assert (b'href="/report/1/"' in response.content) == matches
 
 
 def test_report_detail(admin_client, report_obj):
@@ -28,11 +39,32 @@ def test_report_detail(admin_client, report_obj):
     assert b'>Debug log<' in response.content
 
 
-def test_bin_list(admin_client, bin_obj):
-    response = admin_client.get(urls.reverse('bin_list'))
+@pytest.mark.parametrize('query, matches', [
+    (None, True),
+    ('test', True),
+    ('Test', True),
+    ('', True),
+    ('blah', False),
+])
+def test_bin_list(admin_client, bin_obj, query, matches):
+    url = urls.reverse('bin_list')
+    if query is not None:
+        url += '?q={}'.format(query)
+
+    response = admin_client.get(url)
     assert response.status_code == 200
-    assert b'>testbin<' in response.content
-    assert b'href="/bin/1/"' in response.content
+    assert (b'>testbin<' in response.content) == matches
+
+
+@pytest.mark.parametrize('scope', ['Reports', 'Bins'])
+def test_search_dispatch(admin_client, bin_obj, report_obj, scope):
+    url = urls.reverse('search_dispatch') + '?q=foo&scope={}'.format(scope)
+    message = 'No {} found'.format(scope.lower())
+
+    response = admin_client.get(url)
+
+    assert response.status_code == 200
+    assert message in response.content.decode()
 
 
 def test_bin_detail(admin_client, bin_obj):
