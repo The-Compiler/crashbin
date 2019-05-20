21caef64964395867b15ff3bd4b8de0d5907becb
Florian Bruhin <me@the-compiler.org>
2019-05-20 15:33:30 +0200

    Add report_saved signal

--- crashbin_app/signals.py
+++ crashbin_app/signals.py
@@ -2,18 +2,25 @@ import logging
 
 from django.dispatch import Signal, receiver
 import django_mailbox.signals
+import django.db.models.signals
 
 from crashbin_app import models
 
 
-new_report = Signal(providing_args=['report', 'bin'])
+new_report = Signal(providing_args=['report'])
 
 
 @receiver(django_mailbox.signals.message_received)
-def process_incoming_mail(message, **kwargs):  # pylint: disable=unused-argument
+def process_incoming_mail(message, **kwargs) -> None:  # pylint: disable=unused-argument
     try:
         report = models.Report.for_mail_subject(message.subject)
     except models.InvalidMailError as ex:
         logging.error(str(ex))
         return
     models.IncomingMessage.objects.create(mail=message, report=report)
+
+
+@receiver(django.db.models.signals.post_save, sender=models.Report)
+def report_saved(created: bool, instance: models.Report, **kwargs) -> None:
+    if created:
+        new_report.send(sender=None, report=instance)
