a6097f46d0f13c3a13f03031393ef35d604d8c4b
Florian Bruhin <me@the-compiler.org>
2019-05-23 14:55:35 +0200

    Use a template tag for label styling

--- crashbin_app/templatetags/utils.py
+++ crashbin_app/templatetags/utils.py
@@ -1,11 +1,17 @@
 from django import template
 
 from crashbin_app import utils
+from crashbin_app.models import Label
 
 
 register = template.Library()
 
 
 @register.simple_tag
-def custom_css():
+def custom_css() -> str:
     return utils.config.CUSTOM_CSS
+
+
+@register.simple_tag
+def label_style(label: Label) -> str:
+    return 'background-color: {} !important; color: white !important;'.format(label.color)
