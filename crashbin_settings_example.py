33f4f0596e5b50f61c750f926db5f5601848bc3f
Florian Bruhin <me@the-compiler.org>
2019-05-23 18:17:55 +0200

    Remove one color from suggestions

--- crashbin_settings_example.py
+++ crashbin_settings_example.py
@@ -48,6 +48,5 @@ INBOX_BIN = "Inbox"
 # Suggested colors for labels
 LABEL_COLORS = ['#0033CC', '#428BCA', '#44AD8E', '#A8D695', '#5CB85C',
                 '#69D100', '#004E00', '#34495E', '#7F8C8D', '#A295D6',
-                '#5843AD', '#8E44AD', '#FFECDB', '#AD4363', '#D10069',
-                '#CC0033', '#FF0000', '#D9534F', '#D1D100', '#F0AD4E',
-                '#AD8D43']
+                '#5843AD', '#8E44AD', '#AD4363', '#D10069', '#CC0033',
+                '#FF0000', '#D9534F', '#D1D100', '#F0AD4E', '#AD8D43']
